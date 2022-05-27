d332b0095daa8ebc6c045176e40d41a423fe8809
henricksone <erik.henrickson@om.org>
2022-05-27 11:25:21 -0400

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-// Call Hello World
-function HelloWorld(){
-    alert("hello World");
+// main
+function main(){
+    displayResults(checkString(getString()));
+}
+
+// Get String
+function getString(){
+    return document.getElementById("inputphrase").value;
+}
+
+// Logic
+function checkString(userString){
+    let revString = [];
+    let counter = 0;
+    // reverse using a for loop
+    for (let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];        
+    }
+    // check each position of userString and revString to be same
+    // add 1 if true
+    for (let j = 0; j < userString.length; j++) {
+        counter += (revString[j] == userString[j] ? 1 : 0);
+    }
+    // return the userString, revString, and palindrome: T/F
+    return [userString, revString, (counter == userString.length ? true : false)];
+}
+
+// Display
+function displayResults(results){
+    let msg = (results[2] == true ? 
+        `<p>Success: <span class="text-danger">${results[0]}</span> is a Palindrome!</p>` 
+        : `<p>Failure: <span class="text-danger">${results[0]}</span> is not a Palindrome!<br>
+        Your reversed String is: <span class="text-danger">${results[1]}</span></p>`);
+    document.getElementById("results").innerHTML = `<h3>Results</h3><br>${msg}`;
 }
